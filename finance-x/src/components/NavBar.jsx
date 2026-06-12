0c1546f566c25752025c72951473bc750a738d30
Stark-jpg-web <adelashrafmuhammed@gmail.com>
2026-06-12 03:19:19 +0300

    Created the reducer for the finance context

--- finance-x/src/components/NavBar.jsx
+++ finance-x/src/components/NavBar.jsx
@@ -13,12 +13,13 @@ export default function NavBar() {
 
     return(
     <>
-    <nav>
+    <nav className="nav bg-surface text-text-secondary   text-sm flex flex-col items-center gap-4 p-4"> 
+        <h2 className="logo text-text-primary text-xl">Finance-X</h2>
 {links.map(link =>(
     <NavLink
     key={link.to}
     to={link.to}
-    className={({isActive}) => isActive ? "active" : ""}
+    className={({isActive})  => isActive ? "bg-border px-4 py-1.5 rounded-md text-text-primary" : ""}
     >   {link.label}
     </NavLink>
 ))}
