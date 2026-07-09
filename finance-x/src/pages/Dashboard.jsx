9ea4c8cb177e538b82845a7a084bf4422368ba7f
Stark-jpg-web <adelashrafmuhammed@gmail.com>
2026-07-09 16:20:33 +0300

    Started working on the UI/UX design added some new features to the dashboard and updated the package dependencies, added a barchart using recharts library to visualize financial data and improved the overall user experience by enhancing the layout and styling of the application.

--- finance-x/src/pages/Dashboard.jsx
+++ finance-x/src/pages/Dashboard.jsx
@@ -1,16 +1,87 @@
+import { Bar, BarChart, XAxis, YAxis } from 'recharts';
 import {useContext} from 'react'
 import {FinanceContext} from '../context/FinanceContext'
+import { useCharData } from '../hooks/useCharData';
 
 
 export default function Dashboard() {
 
-    const { transactions, dispatch } = useContext(FinanceContext,[])
+    const { transactions, dispatch } = useContext(FinanceContext)
+    const { monthlyLineChartData } = useCharData()
+
+    // Bar chart
+    const formatAxisTick = (value) => {
+      return `*${value}*`;
+    };
+
+    const renderIncomeLabel = ({ x, y, width, value }) => {
+      if (value == null) return null;
+      return (
+        <text x={x + width / 2} y={y} fill="#10b981" textAnchor="middle" dy={-16}>
+          Income
+        </text>
+      );
+    };
+
+    const renderExpenseLabel = ({ x, y, width, value }) => {
+      if (value == null) return null;
+      return (
+        <text x={x + width / 2} y={y} fill="#D85A30" textAnchor="middle" dy={-16}>
+          Expense
+        </text>
+      );
+    };
+
 
     return(
-<>
-<h1>Dashboard</h1>
 
-</>
+<div className="dashboard bg-bg text-text-primary p-5 flex flex-col gap-5">
+
+
+<h3 className="text-2xl font-bold mb-5 text-text-primary">Overview</h3>
+
+
+<div className="metric-container grid grid-cols-3 gap-3 mb-5">
+
+<div className="metric-card ">
+    <h3 className="card-label text-income ">$ TOTAL INCOME</h3>
+    <p className='card-value text-income'>$ 0000</p>
+</div>
+
+<div className="metric-card">
+    <h3 className="card-label text-expense">TOTAL EXPENSES</h3>
+    <p className="card-value text-expense">$ 0000</p>
+</div>
+
+<div className="metric-card ">
+    <h3 className="card-label ">BALANCE</h3>
+    <p className="card-value ">$ 0000</p> 
+</div>
+</div> 
+
+<div className="chart-container bg-surface p-5 rounded-lg shadow-md flex flex-col items-center gap-5">
+
+<h3 className="text-2xl font-bold mb-5  self-start ">Monthly Income vs  Expenses</h3>
+
+ <BarChart width={600} height={500} data={monthlyLineChartData} margin={{ top: 80, right: 50, left: 50, bottom: 80 }}>
+      <XAxis
+        dataKey="month"
+        tickFormatter={formatAxisTick}
+        tickMargin={20}
+        label={{ position: 'insideBottomRight', value: 'Month', offset: -20, dy: 5 }}
+      />
+      <YAxis label={{ position: 'insideTopLeft', value: 'Amount', angle: -90, dy: 42, dx: -10 }} />
+      <Bar dataKey="income" name="Income" fill="#10b981" label={renderIncomeLabel} />
+      <Bar dataKey="expense" name="Expense" fill="#D85A30" label={renderExpenseLabel} />
+    </BarChart>
+
+</div>
+
+</div>  
+
+
+
+
     )
 
 
